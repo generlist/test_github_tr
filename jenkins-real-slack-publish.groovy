a5d3ccbf24989f5eaa844f335098e804c07c2574
hyunbung.shin <hyunbung.shin@navercorp.com>
2020-11-03 16:23:25 +0900

    테스트트트트트트트트트트dd

--- jenkins-real-slack-publish.groovy
+++ jenkins-real-slack-publish.groovy
@@ -157,14 +157,16 @@ try {
     manager.listener.logger.println "[Fail StackTrace]: ${ae}"
     //notifySlack("LineTV ${appVersion()} Real 배포 공유", slackNotificationChannel, failNotification)
     manager.listener.logger.println("kkkkkk() =${e}")
-    manager.currentBuild.result = 'ABORTED'
-    error('Stopping early…')
+    Jenkins .instance.getItemByFullName("JobName")
+            .getBuildByNumber(JobNumber)
+            .finish(hudson.model.Result.ABORTED, new java.io.IOException("Aborting build"));
 } catch (e) {
     manager.listener.logger.println "[Fail StackTrace]: ${e}"
     //notifySlack("LineTV ${appVersion()} Real 배포 공유", slackNotificationChannel, failNotification)
     manager.listener.logger.println("kkkkkk() =${e}")
-    manager.currentBuild.result = 'ABORTED'
-    error('Stopping early…')
+    Jenkins .instance.getItemByFullName("JobName")
+            .getBuildByNumber(JobNumber)
+            .finish(hudson.model.Result.ABORTED, new java.io.IOException("Aborting build"));
 
 }
 
