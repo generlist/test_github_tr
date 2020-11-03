a1bd61cd7992b9af583a85f858de1187a004922b
hyunbung.shin <hyunbung.shin@navercorp.com>
2020-11-03 18:28:11 +0900

    테스트트트트트트트트트트dd

--- jenkins-real-slack-publish.groovy
+++ jenkins-real-slack-publish.groovy
@@ -5,29 +5,16 @@ import groovy.transform.Field
 //앱 버전을 가져온다.
 
 @Field final String slackNotificationChannel = 'android-app-release'
+
 @Field def failErrorNotification = [
         [
-                fallback: "Plain-text summary of the attachment.",
+                fallback: "summary of the attachment.",
                 color: "#2eb886",
-                pretext: "Optional text that appears above the attachment block",
-                author_name: "Bobby Tables",
-                author_link: "http://flickr.com/bobby/",
-                author_icon: "http://flickr.com/icons/bobby.jpg",
-                title: "Slack API Documentation",
-                title_link: "https://api.slack.com/",
-                text: "Optional text that appears within the attachment",
-                fields: [
-                        [
-                                title: "Priority",
-                                value: "High",
-                                short: false
-                        ]
-                ],
-                image_url: "http://my-website.com/path/to/image.jpg",
-                thumb_url: "http://example.com/path/to/thumb.png",
-                footer: "Slack API",
-                footer_icon: "https://platform.slack-edge.com/img/default_application_icon.png",
-                ts: 123456789
+                pretext: "Notification Error",
+                title: "Jenkins Build Error",
+                text: "*<${getBuildUrl()}|빌드 바로 가기>*",
+                "mrkdwn_in": ["text"],
+
         ]
 ]
 def getGitAuthor = {
